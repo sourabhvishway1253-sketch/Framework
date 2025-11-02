a73ebc269797655e99315092dac8c3b3731e97a5
sourabhvishway1253-sketch <sourabhvishway1253@gmail.com>
2025-11-02 10:42:36 +0530

    Update Create_Campaign_With_ProductTest.java

--- Revise/src/test/java/Campaign/Create_Campaign_With_ProductTest.java
+++ Revise/src/test/java/Campaign/Create_Campaign_With_ProductTest.java
@@ -33,7 +33,7 @@ import Object_Repository.Create_Campaign_Page;
 import Object_Repository.Home_Page;
 
 public class Create_Campaign_With_ProductTest extends Base_Class {
-
+//Pulling_Back
 	@Test(groups="regressionTest")
 	public void Create_Campaign_With_ProductTest()  throws Throwable {
 		
